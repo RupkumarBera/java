5469f3647c4c5a5c5ab682d7a620fc817c9feb7d
Rupkumar Bera <78893621+RupkumarBera@users.noreply.github.com>
2024-04-10 01:36:57 +0530

    Add files via upload

---
+++ JAVA_Codes/GCD9.java
@@ -0,0 +1,27 @@
+
+    import java.util.Scanner;
+
+public class GCD9{
+    
+    public static int findGCD(int a, int b) {
+        while (b != 0) {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+        return a;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter two numbers:");
+        int num1 = scanner.nextInt();
+        int num2 = scanner.nextInt();
+        scanner.close();
+
+        int gcd = findGCD(num1, num2);
+        System.out.println("GCD of " + num1 + " and " + num2 + " is: " + gcd);
+    }
+}
+
+
